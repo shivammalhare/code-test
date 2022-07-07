5cbf8240b7edfa887ac9f5cbe1ef8cdeebc9e972
Shivam Malhare <shivam@collabsports.com>
2022-07-07 15:51:41 +0530

    First Deploy Test

---
+++ application/views/footer.php
@@ -0,0 +1,83 @@
+<div class="container-fluid bg-img text-secondary" style="margin-top: 90px">
+        <div class="container">
+            <div class="row gx-5">
+                <div class="col-lg-4 col-md-6 mb-lg-n5">
+                    <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary border-inner p-4">
+                        <a href="index.html" class="navbar-brand">
+                            <h1 class="m-0 text-uppercase text-white"><i class=""></i>Vintage Delight</h1>
+                            <img class="img-responsive" src="http://[::1]/Vintage_Delight/assets/img/vd_logo.png" style= "height:300px ;width:300px;object-fit:">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-6">
+                    <div class="row gx-5">
+                        <div class="col-lg-4 col-md-12 pt-5 mb-5">
+                            <h4 class="text-primary text-uppercase mb-4">Get In Touch</h4>
+                            <div class="d-flex mb-2">
+                                <i class="bi bi-geo-alt text-primary me-2"></i>
+                                <p class="mb-0">A/P Shivapur , Dist - Pune 412205, near datta mandir, Maharashtra</p>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <i class="bi bi-envelope-open text-primary me-2"></i>
+                                <a href="mailto:vintagedelight@gmail.com"><p class="mb-0" style="color:white;">vintagedelight@gmail.com</p></a>
+                            </div>
+                            <div class="d-flex mb-2">
+                                <i class="bi bi-telephone text-primary me-2"></i>
+                                <a href="tel:7218901587"><p class="mb-0" style="color:white;" >+91 72189 01587</p>
+                            </div>
+                            <div class="d-flex mt-4">
+                                <a class="btn btn-lg btn-primary btn-lg-square border-inner rounded-0 me-2" href="#"><i class="fab fa-twitter fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square border-inner rounded-0 me-2" href="#"><i class="fab fa-facebook-f fw-normal"></i></a>
+                                <a class="btn btn-lg btn-primary btn-lg-square border-inner rounded-0 me-2" href="#"><i class="fab fa-linkedin-in fw-normal"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
+                            <h4 class="text-primary text-uppercase mb-4">Quick Links</h4>
+                            <div class="d-flex flex-column justify-content-start">
+                                <a class="text-secondary mb-2" href="<?php echo base_url().'index.php/main/index';?>"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
+                                <a class="text-secondary mb-2" href="<?php echo base_url().'index.php/main/about';?>"><i class="bi bi-arrow-right text-primary me-2"></i>About Us</a>
+                                <a class="text-secondary mb-2" href="<?php echo base_url().'index.php/main/services';?>"><i class="bi bi-arrow-right text-primary me-2"></i>Our Services</a>
+                                <a class="text-secondary mb-2" href="<?php echo base_url().'index.php/main/team';?>"><i class="bi bi-arrow-right text-primary me-2"></i>Meet The Team</a>
+                                <a class="text-secondary mb-2" href="<?php echo base_url().'index.php/main/blog';?>"><i class="bi bi-arrow-right text-primary me-2"></i>Latest Blog</a>
+                                <a class="text-secondary" href="<?php echo base_url().'index.php/main/contact';?>"><i class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-secondary py-4" style="background: #111111;">
+        <div class="container text-center">
+            <p class="mb-0"> &copy; <a class="text-white " >2022 Vintage Delight Cakes .   All Rights Reserved. 
+			
+			
+			<a class="text-white border-bottom" href=""></a></p>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary border-inner py-3 fs-4 back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src=" <?php echo base_url().'assets/lib/easing/easing.min.js';?>"></script>
+    <script src=" <?php echo base_url().'assets/lib/waypoints/waypoints.min.js';?>"></script>
+    <script src=" <?php echo base_url().'assets/lib/counterup/counterup.min.js';?>"></script>
+    <script src=" <?php echo base_url().'assets/lib/owlcarousel/owl.carousel.min.js';?>"></script>
+    <!-- <script src=" <?php echo base_url().'assets/lib/isotope/isotope.pkgd.min.js';?>"></script>
+    <script src=" <?php echo base_url().'assets/lib/lightbox/js/lightbox.min.js';?>"></script>
+    <script src=" <?php echo base_url().'assets/lib/touchSwipe/jquery.touchSwipe.min.js';?>"></script>
+     -->
+
+    <!-- Template Javascript -->
+    
+    <script src=" <?php echo base_url().'assets/js/main.js';?>"></script>
+</body>
+
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Vintage Delight - A Cake Shop</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <!-- <link href="<?php echo base_url().'assets/img/favicon.ico';?>" rel="icon"> -->
+    
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Oswald:wght@500;600;700&family=Pacifico&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="<?php echo base_url().'assets/lib/owlcarousel/assets/owl.carousel.min.css';?>" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="<?php echo base_url().'assets/css/bootstrap.min.css';?>" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="<?php echo base_url().'assets/css/style.css';?>" rel="stylesheet">
+</head>
+
+
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid px-0 d-none d-lg-block">
+        <div class="row gx-0">
+            <div class="col-lg-4 text-center bg-secondary py-3">
+                <div class="d-inline-flex align-items-center justify-content-center">
+                    <i class="bi bi-envelope fs-1 text-primary me-3"></i>
+                    <div class="text-start">
+                        <h6 class="text-uppercase mb-1">Email Us</h6>
+                        <a href="mailto:vintagedelight@gmail.com"><span>vintagedelight@gmail.com</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 text-center bg-primary border-inner py-3">
+                <div class="d-inline-flex align-items-center justify-content-center">
+                    <a href="index.html" class="navbar-brand">
+                        <h1 class="m-0 text-uppercase text-white"><i class="fa fa-birthday-cake fs-1 text-dark me-3"></i>Vintage Delight</h1>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 text-center bg-secondary py-3">
+                <div class="d-inline-flex align-items-center justify-content-center">
+                    <i class="bi bi-phone-vibrate fs-1 text-primary me-3"></i>
+                    <div class="text-start">
+                        <h6 class="text-uppercase mb-1">Call Us</h6>
+                        <a href="tel:7218901587"> <span>+91 72189 01587</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-0">
+        <a href="index.html" class="navbar-brand d-block d-lg-none">
+            <h1 class="m-0 text-uppercase text-white"><i class="fa fa-birthday-cake fs-1 text-primary me-3"></i>Vintage Delight</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto mx-lg-auto py-0">
+                <a href="<?php echo base_url();?>" class="nav-item nav-link active">Home</a>
+                <a href="<?php echo base_url().'about';?>" class="nav-item nav-link">About Us</a>
+                <a href="<?php echo base_url().'menu';?>" class="nav-item nav-link">Menu & Pricing</a>
+                <a href="<?php echo base_url().'team';?>" class="nav-item nav-link">Master Chefs</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="<?php echo base_url().'service';?>" class="dropdown-item">Our Service</a>
+                        <a href="<?php echo base_url().'testimonial';?>" class="dropdown-item">Testimonial</a>
+                    </div>
+                </div>
+                <a href="<?php echo base_url().'contact';?>" class="nav-item nav-link">Contact Us</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+   
