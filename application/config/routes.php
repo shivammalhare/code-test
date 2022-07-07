21611abfa9d5053fb78bc1f31284f59bf5dce696
Shivam Malhare <shivam@collabsports.com>
2022-07-07 16:04:14 +0530

    Second Deploy Test

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['home'] = 'main/index';
+$route['about']   = 'main/about';
+$route['menu']   = 'main/menu';
+$route['team']  = 'main/team';
+$route['service']   = 'main/service';
+$route['testimonial']   = 'main/testimonial';
+$route['contact']  = 'main/contact';
